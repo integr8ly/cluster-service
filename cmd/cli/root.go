55a850bab4a406a8e6ebdaaa0896e44c9ca5a013
Aiden Keating <akeating@redhat.com>
2020-03-11 13:48:27 +0000

    ensure cluster-service is the name of binary in help

--- cmd/cli/root.go
+++ cmd/cli/root.go
@@ -20,7 +20,7 @@ var logger = logrus.WithField("service", "cluster_service")
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "cli",
+	Use:   "cluster-service",
 	Short: "A brief description of your application",
 	Long: `A longer description that spans multiple lines and likely contains
 examples and usage of using your application. For example:
